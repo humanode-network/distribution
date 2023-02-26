d76a6923871205a961b6e6de7bbf15aea4b60f52
MOZGIII <mike-n@narod.ru>
2023-02-27 03:03:49 +0530

    Add the main repo as the built-in source

--- crates/built-in-sources/src/lib.rs
+++ crates/built-in-sources/src/lib.rs
@@ -4,4 +4,4 @@
 pub const MANIFEST_URLS: &[&str] = &[];
 
 /// The built-in Repo URLs.
-pub const REPO_URLS: &[&str] = &[];
+pub const REPO_URLS: &[&str] = &["https://manifests.launcher.humanode.io/main.yml"];
